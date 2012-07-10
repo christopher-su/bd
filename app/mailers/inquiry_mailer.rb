9e005b9f6620f97afd18c79a7ec642b77167288b
scchristopher <scchristopher.su@gmail.com>
2012-07-10 23:33:36 +0800

    7.10

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,10 +1,10 @@
 class InquiryMailer < ActionMailer::Base
- default :from => "BrosisDesign-webmaster<brosisdesign.webmaster@gmail.com>"
-
+ #default :from => "BrosisDesign-webmaster<brosisdesign.webmaster@gmail.com>"
+  default :from => "BrosisDesign-webmaster<system@brosisdesign.com>"
 
   def have_new_inquiry(inquiry)
     @inquiry = inquiry
-    mail(:to => "BrosisDesign.sales@gmail.com",:cc => "BrosisDesign.info@gmail.com,Brosisdesign.design@gmail.com,Brosisdesign.service@gmail.com" ,:subject => "Brosisdesgin have new inquiry")
+    mail(:to => "sales@brosisdesign.com",:cc => "services@brosisdesign.com,design@brosisdesign.com" ,:subject => "Brosisdesgin have new inquiry")
   end
 
   def inquiry_comfirm(inquiry)
--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,18 @@
 ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => "gmail.com",
-  :user_name            => "brosisdesign.webmaster@gmail.com",
-  :password             => "google_pass",
-  :authentication       => "plain",
-  :enable_starttls_auto => true
+  #:address              => "smtp.gmail.com",
+  #:port                 => 587,
+  #:domain               => "gmail.com",
+  #:user_name            => "brosisdesign.webmaster@gmail.com",
+  #:password             => "google_pass",
+  #:authentication       => "plain",
+  #:enable_starttls_auto => true
+
+
+    :address              => "smtp.ym.163.com",
+    :port                 => 25,
+    :domain               => "ym.163.com",
+    :user_name            => "system@brosisdesign.com",
+    :password             => "sc_pass",
+    :authentication       => "plain",
+    :enable_starttls_auto => true
 }
